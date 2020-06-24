e2c0d17f606915632f69645199d1a7a469fe009a
Jingbei Li <i@jingbei.li>
2020-06-24 18:31:02 +0800

    fixed update-pacman-conf.sh

--- update-pacman-conf.sh
+++ update-pacman-conf.sh
@@ -1,34 +1,41 @@
 #!/bin/sh
-set -e
+set -ex
 
-cp keyrings/archlinuxarm* /usr/share/devtools/ || :
-fakeroot pacman-key --gpgdir gnupg --init
-fakeroot pacman-key --gpgdir gnupg --populate archlinuxarm || :
+cp keyrings/archlinuxarm* /usr/share/pacman/keyrings || :
 
 for arch in armv6h armv7h aarch64
 do
-	mkdir -p $arch/{db,root}
+	gpgdir=$arch/gnupg
 	cachedir=/var/cache/pacman/pkg/$arch
+
+	mkdir -p $arch/{db,root} $cachedir
+
 	echo [options] > $arch/pacman.conf
 	echo "Architecture = $arch" >> $arch/pacman.conf
 	echo "[core]" >> $arch/pacman.conf
 	echo "Include = $(realpath mirrorlist)" >> $arch/pacman.conf
 
-	fakeroot pacman -Sy --config $arch/pacman.conf --dbpath $arch/db --gpgdir gnupg
-	fakeroot pacman -Sdd --config $arch/pacman.conf --dbpath $arch/db --gpgdir gnupg --root $arch/root --cachedir $cachedir --needed --noconfirm pacman
+	pacman-key --config $arch/pacman.conf --gpgdir $gpgdir --init
+	pacman-key --config $arch/pacman.conf --gpgdir $gpgdir --populate archlinuxarm
+
+	pacman -Sy --config $arch/pacman.conf --dbpath $arch/db --gpgdir $gpgdir
+	pacman -Sdd --config $arch/pacman.conf --dbpath $arch/db --gpgdir $gpgdir --root $arch/root --cachedir $cachedir --needed --noconfirm pacman
 
 	cp $arch/root/etc/makepkg.conf makepkg-$arch.conf
 	cp $arch/root/etc/pacman.conf pacman-extra-$arch.conf
 
 	if [ "$arch" = "aarch64" ]
 	then
-		fakeroot pacman -Sdd --config $arch/pacman.conf --dbpath $arch/db --gpgdir gnupg --root $arch/root --cachedir $cachedir --needed --noconfirm pacman-mirrorlist
-		fakeroot pacman -Swdd --config $arch/pacman.conf --dbpath $arch/db --gpgdir gnupg --cachedir $cachedir --noconfirm archlinuxarm-keyring
-		fakeroot pacman -Sc --config $arch/pacman.conf --dbpath $arch/db --gpgdir gnupg --cachedir $cachedir --noconfirm
+		pacman -Sdd --config $arch/pacman.conf --dbpath $arch/db --gpgdir $gpgdir --root $arch/root --cachedir $cachedir --needed --noconfirm pacman-mirrorlist
+		pacman -Swdd --config $arch/pacman.conf --dbpath $arch/db --gpgdir $gpgdir --cachedir $cachedir --needed --noconfirm archlinuxarm-keyring
+		package=$(pacman -Sp --config $arch/pacman.conf --dbpath $arch/db --cachedir $cachedir archlinuxarm-keyring)
+		package=${package#file://}
 		rm -rf keyrings
-		xz -d $arch/cache/archlinuxarm-keyring-*.pkg.tar.xz --stdout | tar xvf - usr/share/pacman/keyrings --strip 3
+		xz -d $package --stdout | tar xvf - usr/share/pacman/keyrings --strip 3
 		cp $arch/root/etc/pacman.d/mirrorlist .
+		mv $package /tmp
 	fi
 
-	fakeroot pacman -Sc --config $arch/pacman.conf --dbpath $arch/db --gpgdir gnupg --cachedir $cachedir --noconfirm
+	pacman -Sc --config $arch/pacman.conf --dbpath $arch/db --gpgdir $gpgdir --cachedir $cachedir --noconfirm
+	[ "$arch" = "aarch64" ] && mv /tmp/$(basename $package) $package || :
 done
